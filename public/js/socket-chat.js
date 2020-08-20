8be19128ab6aba0f7a6cfca175c8c3ffac77ee83
ivanxx <ivan@monodosis.com>
2020-08-20 13:30:02 +0200

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,9 +1,10 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
@@ -11,12 +12,16 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados ', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -27,28 +32,29 @@ socket.on('disconnect', function() {
 });
 
 
-
 // Enviar información
 // socket.emit('crearMensaje', {
-//    usuario: 'Fernando',
-//    mensaje: 'Hola Mundo'
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
 // }, function(resp) {
-//    console.log('respuesta server: ', resp);
+//     console.log('respuesta server: ', resp);
 // });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado', mensaje);
-})
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
